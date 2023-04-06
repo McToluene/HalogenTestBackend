3835df6e028e4fb3c43120a1b761901b8b5d6183
McToluene <xclusiveniggaz10@live.com>
2023-04-06 10:53:22 +0100

    Navbar added

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { GenerateComponent } from './generate/generate.component';
 import { ResultComponent } from './result/result.component';
 import { DownloadComponent } from './download/download.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, UploadComponent, GenerateComponent, ResultComponent, DownloadComponent],
+  declarations: [AppComponent, UploadComponent, GenerateComponent, ResultComponent, DownloadComponent, NavbarComponent],
   imports: [BrowserModule, NgbModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
